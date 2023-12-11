f523ef2c962b4c2016a38572c900c3230a0d802b
Martin Harvan <martinhrvn@gmail.com>
2023-12-11 16:22:46 +0100

    Day11 part 2

--- src/day11/Day11.kt
+++ src/day11/Day11.kt
@@ -6,50 +6,58 @@ import println
 import readInput
 import kotlin.math.abs
 
+data class Point(val x: Long, val y: Long)
 
-class Day11(val input: List<String>) {
+data class GalaxyMap(val input: List<String>, val expansion: Long) {
+    val colsToMultipy = getEmptyCols()
+    val rowsToMultipy = getEmptyRows()
 
-    fun doubleRows(input: List<String>): List<String> {
-        val rowsToDouble = input.map { !it.contains("#") }.reversed()
-        val colsToDouble = IntArray(input[0].length) { 1 }
-
-        input.forEachIndexed { row, rowElm ->
-        rowElm.forEachIndexed {  col, elm -> if (elm == '#') {
-            colsToDouble[col] = 0
-        }
-        }
+    fun getEmptyRows(): List<Int> {
+        return input.mapIndexedNotNull { i, r -> if (!r.contains("#")) i else null}
     }
-        val doubledRows = input.reversed().flatMapIndexed { index, row ->
-            if (rowsToDouble[index]) listOf(row, row) else listOf(row)
-        }
 
-        val reversedCols = colsToDouble.reversed()
-        // Doubling columns
-        val doubledCols = doubledRows.map { row ->
-            row.reversed().foldIndexed("") { index, acc, c ->
-                acc + c.toString().repeat(if (reversedCols[index] == 1) 2 else 1)
+    fun getEmptyCols(): List<Int> {
+        return input[0].indices.filter { i -> input.none { line -> line[i] == '#' }}
+    }
+
+    fun getGalaxies(): List<Point> {
+        return input.flatMapIndexed { y, row ->
+            row.mapIndexedNotNull { x, elm ->
+                val expandedX = colsToMultipy.count { x > it} * (expansion - 1)
+                val expandedY = rowsToMultipy.count { y > it} * (expansion - 1)
+                if (elm == '#')
+                    Point(x.toLong() + expandedX, y.toLong() + expandedY)
+                else
+                    null
             }
         }
-        return doubledCols.map { row -> row.reversed() }.reversed()
     }
 
-    fun getGalaxies(input: List<String>): List<Coord> {
-        return input.flatMapIndexed { y, row ->
-            row.mapIndexedNotNull { x, elm -> if (elm == '#') Coord(x, y) else null }
-        }
+    fun getGalaxyPairs(): Sequence<Pair<Point, Point>> {
+        return elementPairs(getGalaxies())
     }
 
-    fun part1(): Int {
-        val doubled = doubleRows(input)
-        val galaxies = getGalaxies(doubled)
-        return elementPairs(galaxies).sumOf  { (g1,g2) ->
-            abs(g2.first - g1.first ) + abs(g2.second-g1.second)
-        }
+    fun getGalaxyDistance(g1: Point, g2: Point): Long {
+        return abs(g2.x - g1.x) + abs(g2.y - g1.y)
+    }
 
+    fun getSumOfDistances(): Long {
+        return getGalaxyPairs().sumOf { (g1, g2) -> getGalaxyDistance(g1, g2) }
+    }
+}
+
+class Day11(val input: List<String>) {
+    fun part1(): Long {
+       return  GalaxyMap(input, 2).getSumOfDistances()
+    }
+
+    fun part2(): Long {
+        return  GalaxyMap(input, 1000000).getSumOfDistances()
     }
 }
 
 fun main() {
     val day11 = Day11(readInput("day11/Day11"))
     day11.part1().println()
+    day11.part2().println()
 }
